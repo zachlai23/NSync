4fd68c84cb1246971daf9d463c0ee5dd96ade911
Zachary Lai <zachlai52@gmail.com>
2024-10-22 17:18:53 -0700

    Initial Commit

---
+++ NSync/NSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSyncApp.swift
+//  NSync
+//
+//  Created by Zachary Lai on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NSyncTests/NSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  NSyncTests.swift
+//  NSyncTests
+//
+//  Created by Zachary Lai on 10/22/24.
+//
+
+import Testing
+@testable import NSync
+
+struct NSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
